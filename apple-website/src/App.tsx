9906764132f106cfe6d5f665d331b2c4ee668ebf
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2024-06-19 14:14:03 +0200

    Hero section of iphone site

--- apple-website/src/App.tsx
+++ apple-website/src/App.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
 
   return <main className="bg-black">
     <Navbar />
+    <Hero />
   </main>;
 }
 
