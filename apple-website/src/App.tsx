dc8470b5909f53a737942a9692c6f1680b10234e
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2024-06-19 15:19:16 +0200

    Video Carousel

--- apple-website/src/App.tsx
+++ apple-website/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 function App() {
 
   return <main className="bg-black">
     <Navbar />
     <Hero />
+    <Highlights />
   </main>;
 }
 
